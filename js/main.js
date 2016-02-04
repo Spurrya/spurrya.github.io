b3fa9b8c6f49a26e24f4f477ba5a7ba4c1ad06c8
Spurrya Jaggi <spurrya@gmail.com>
2016-02-03 21:16:34 -0500

    just refreshing website on sending form'

--- js/main.js
+++ js/main.js
@@ -129,13 +129,6 @@ $(".loadPost").on('click', function(event){
 
 //==============___Contact Form Validator and Ajax Sender___================
 
-// $("#submittedForm").click(function(){
-//   $("#contactSuccess").dialog();
-// });
-
-if (window.location.hash === "#thanks") {
-    $("#contactSuccess").dialog();
-}
 //   $("#contactForm").validate({
 //     submitHandler: function(form) {
 //       $.ajax({
